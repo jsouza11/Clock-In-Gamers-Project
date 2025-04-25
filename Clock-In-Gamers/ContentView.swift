8cddbfe2ff555b0eaecce47fbf8103b9ab744adb
jsouza11 <143134566+jsouza11@users.noreply.github.com>
2025-04-24 19:41:02 -0700

    Testing

--- Clock-In-Gamers/ContentView.swift
+++ Clock-In-Gamers/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Project")
+            Text("Project Testing")
         }
         .padding()
     }
